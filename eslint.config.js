b2d8b21f1ec35fb616658ff11a81143e5934a982
Ly Tran <tranthilyly.khtnk15@gmail.com>
2024-10-17 17:13:08 +0300

    configure: add prettier format on save

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
@@ -8,4 +8,9 @@ export default [
   { ignores: ['**/node_modules/**', '**/migrations/**'] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-]
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
